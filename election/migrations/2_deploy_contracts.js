25fbe672b331221f2614cf2647372147d75dcd58
Giwansh Aryan <giwansharyan@pop-os.localdomain>
2021-01-08 19:21:25 +0530

    Final commit

--- election/migrations/2_deploy_contracts.js
+++ election/migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 var Election = artifacts.require("./Election.sol");
-//here artifacts represents the contract abstraction that is specific to truffle
-
 
 module.exports = function(deployer) {
   deployer.deploy(Election);
 };
-
-//this will help us to interact with Election solidity file
-
